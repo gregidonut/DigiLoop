60844e72e22aea93ecb8b914395e8a61667361b4
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-02 12:06:42 +0800

    feat: add list of services to services page
    This is a working draft of the list of services

--- DigiLoop/src/routes/Services/index.tsx
+++ DigiLoop/src/routes/Services/index.tsx
@@ -1,13 +1,38 @@
-import { component$ } from "@builder.io/qwik";
+import {component$} from "@builder.io/qwik";
 
 import Header from "~/components/Header/Header";
 
 export default component$(() => {
     return (
         <>
-            <Header />
+            <Header/>
             <main>
                 <h2>Services</h2>
+                <section>
+                    <ul>
+                        <li>
+                            Social media management
+                        </li>
+                        <li>
+                            Social media ads
+                        </li>
+                        <li>
+                            Branding
+                        </li>
+                        <li>
+                            Web design
+                        </li>
+                        <li>
+                            Film Production
+                        </li>
+                        <li>
+                            3d design
+                        </li>
+                        <li>
+                            Game development
+                        </li>
+                    </ul>
+                </section>
             </main>
         </>
     );
