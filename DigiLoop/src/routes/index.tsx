e6e06c952c9f9502d39b9f18a6f74ddab7b85683
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-08-02 12:32:18 +0800

    feat: update description meta tag content
    Updated with the content of the main page p tag content

--- DigiLoop/src/routes/index.tsx
+++ DigiLoop/src/routes/index.tsx
@@ -1,11 +1,11 @@
-import {component$} from "@builder.io/qwik";
-import type {DocumentHead} from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 import Header from "~/components/Header/Header";
 
 export default component$(() => {
     return (
         <>
-            <Header/>
+            <Header />
             <main>
                 <p class="short-description">
                     Unlock the true potential of your business with our cutting-edge Virtual Assistant services. Our
@@ -23,7 +23,11 @@ export const head: DocumentHead = {
     meta: [
         {
             name: "description",
-            content: "A Virtual Assistant Company",
+            content:
+                "Unlock the true potential of your business with our cutting-edge Virtual Assistant services. " +
+                "Our team of skilled and dedicated professionals is here to provide you with a seamless experience " +
+                "that will elevate your brand to new heights. Whether you are an established enterprise or a budding " +
+                "entrepreneur, our comprehensive suite of services is designed to cater to all your needs.",
         },
     ],
 };
